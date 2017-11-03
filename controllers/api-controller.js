02345b925dbdf33eea2a60d14b7a991174913f0a
Viswanathan, Subin <Subin.Viswanathan@cdk.com>
2017-11-03 10:37:54 +0530

    changes for the mblab connection with callback

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,57 +1,67 @@
 var Todos = require('../models/todo-model');
 var bodyParser = require('body-parser');
 
-module.exports = function (app) {
-  //send the data nicely to api so use middleware
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.get('/api/todo/:uname',function (req,res) {
-      Todos.find({username:req.params.uname},function (err,todos) {
-          if (err) throw err;
+module.exports = function(app) {
 
-          res.send(todos);
-      })
-  });
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.get('/api/todo/:id',function (req,res) {
-      Todos.findById({_id:req.params.id},function (err,todo) {
-          if (err) throw err;
+    app.get('/api/todos/:uname', function(req, res) {
 
-          res.send(todo);
-      });
-  });
+        //var promise = Todos.find({ username: req.params.uname })
 
-  app.post('/api/todo',function (req,res) {
-      if (req.body.id){
-          Todos.findByIdAndUpdate(req.body.id,{
-              todo:req.body.todo,isDone: req.body.isDone, hasAttachment:req.body.hasAttachment
-          },function (err,todo) {
-              if (err) throw err;
+        Todos.find({ username: req.params.uname }, function(err, todos) {
+            if (err) throw err;
 
-              res.send("Succes");
-          });
-      }else {
-          // craete a new
-          var newTodo = Todos({
-              username : "username",
-              todo : req.body.todo,
-              isDone : req.body.isDone,
-              hasAttachment:req.body.hasAttachment
-          });
-          newTodo.save(function (err) {
-              if (err) throw err;
+            res.send(todos);
+        });
 
-              res.send("Succes");
-          });
-      }
-  });
+    });
 
-  app.delete('/api/todo',function (req,res) {
-      Todos.findByIdAndRemove(req.body.id,function (err) {
-          if (err) throw err;
+    app.get('/api/todo/:id', function(req, res) {
 
-          res.send("Succes");
-      })
-  });
-};
+        Todos.findById({ _id: req.params.id }, function(err, todo) {
+            if (err) throw err;
 
+            res.send(todo);
+        });
+
+    });
+
+    app.post('/api/todo', function(req, res) {
+
+        if (req.body.id) {
+            Todos.findByIdAndUpdate(req.body.id, { todo: req.body.todo, isDone: req.body.isDone, hasAttachment: req.body.hasAttachment }, function(err, todo) {
+                if (err) throw err;
+
+                res.send('Success');
+            });
+        }
+
+        else {
+
+            var newTodo = Todos({
+                username: 'test',
+                todo: req.body.todo,
+                isDone: req.body.isDone,
+                hasAttachment: req.body.hasAttachment
+            });
+            newTodo.save(function(err) {
+                if (err) throw err;
+                res.send('Success');
+            });
+
+        }
+
+    });
+
+    app.delete('/api/todo', function(req, res) {
+
+        Todos.findByIdAndRemove(req.body.id, function(err) {
+            if (err) throw err;
+            res.send('Success');
+        })
+
+    });
+
+}
--- index.js
+++ index.js
@@ -10,13 +10,14 @@ var apiController = require('./controllers/api-controller');
 //need tp serve the dist as the angular will bundled by webpack compiled to js so we need to give that path
 app.use(express.static(__dirname + '/public/dist'));
 
-// app.get('/', function(req, res){
-//     res.send('hello world');
-// });
 
 //set connection to moongose db it will be connected always
-//mongoose.connect(config.getDbConnection());
-mongoose.connect(config.getDbConnection(), { useMongoClient: true });
+mongoose.connect(config.getDbConnection(),(err,db) => {
+    if (err) return console.log(err);
+
+    console.log("connected");
+});
+//mongoose.connect(config.getDbConnection(), { useMongoClient: true });
 //mongoose.connection.openUri();
 
 //it will add endpoint check for dev
